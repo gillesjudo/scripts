e436ebf4981d9a829c9a2bbb4338356e99031cf9
Gilles Castro <gillescastro@mailfence.com>
2024-01-24 16:35:07 -0500

    1/24/23 beginning to flush out the netscanner

--- Python/networkscanner.py
+++ Python/networkscanner.py
@@ -3,25 +3,43 @@
 import subprocess
 import re
 from pacCheck import package_checker
+from scapy.all import *
 
 '''This script will require the program netdiscover so we will create
 a function to ensure the netdiscover package is present.'''
 
-package = ['netdiscover', 'nmap', 'scapy']
+package = ['nmap', 'scapy']
 package_checker(package)
+
+userIP = ""
+def myIP():
+    global userIP 
+    global userHW
+    ipcom = subprocess.Popen(('ip', '-4', '-brief', 'address', 'show'), stdout=subprocess.PIPE)
+    ip_Addr = subprocess.check_output(('grep', '-Eo', '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-3][0-9]'), stdin=ipcom.stdout)
+    ipcom.stdout.close()
+    ipLink = subprocess.Popen(('ip', 'link', 'show'), stdout=subprocess.PIPE)
+    greps = subprocess.Popen(('grep','-Eo', 'ether\s([a-f0-9]{2}:?){6}'), stdout=subprocess.PIPE, stdin=ipLink.stdout)
+    ipLink.stdout.close()
+    hw_Addr = subprocess.check_output(('grep','-Eo', '([a-f0-9]{2}:?){6}'),stdin=greps.stdout)
+    greps.stdout.close()
+    userIP = str(ip_Addr.strip().decode('utf-8'))
+    userHW = str(hw_Addr.strip().decode('utf-8'))
+    print('Your IP address is', userIP,'\nYour mac address is', userHW )
+myIP()
 '''
-ipa = subprocess.Popen(('ip', '-4', '-brief', 'address', 'show'), stdout=subprocess.PIPE)
-greps = subprocess.Popen(('grep', '-Eo', '([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-3][0-9]'), stdin=ipa.stdout, stdout=subprocess.PIPE)
-ipa.stdout.close()
-myIP = subprocess.check_output(('grep', '-Eo','([0-9]{1,3}\.){3}[0-9]{1,3}'), stdin=greps.stdout)
-greps.stdout.close()
-print('this is your IP address', myIP.strip().decode('utf-8'))
-print('Lets map out what else is present using your Ip address.')
+Now that I am able to extract the IP I want to be able to extrapolate out into
+the network and determine what else is present. It might be a wise decision to 
+not only use arp to find other hosts but to also include a ping scan if we dont
+mind being noisy. So we will first below create a function to do an arp broadcast.  
+'''
+def layer2NetScan(ip):
+   print(userIP)
+   
+layer2NetScan(userIP)
 
-range = input("What is the subnet range for your network? ")
-ipRange = myIP.strip().decode('utf-8') + range
-
-print(ipRange)
-
-netscan = subprocess.run(('sudo' ,'netdiscover', '-r', ipRange))
+'''I want to give the user 3 options for scanning a network. I want to give a 
+quiet (other users unlikely to be alerted) option, a louder option that will 
+likely alert other users on the network but will be more accurate and a loud and
+detailed scan. I will use 3 different tools to guarantee this happens.  
 '''
--- Python/pacCheck.py
+++ Python/pacCheck.py
@@ -6,25 +6,26 @@ def package_checker(package):
     install=['sudo', 'apt', 'install', package, '-y']
     #The Find and install variables are created to be input into the subprocess.run module
     #Both are the command syntax I will be running later
+    print('[+] Checking Necessary Packages and dependencies.')
     if isinstance(package,list):
         for pack in package:
             find[5] = pack.strip()
             install[3] = pack.strip()
             find_output=subprocess.run(find, capture_output=True, text=True)
-            print(find_output.stdout.strip())
+            #print(find_output.stdout.strip())
             if find_output.stdout.strip() == '':
-                print("It does not seem like", pack, "is installed, will install now.")
+                print("[+] It does not seem like", pack, "is installed, will install now.")
                 subprocess.run(install)
             else:
-                print(pack, "is present.")
+                print("[+]",pack, "is present.")
     else:
         find_output=subprocess.run(find, capture_output=True, text=True)
         print(find_output.stdout.strip())
         if find_output.stdout.strip() == '':
-            print("It does not seem like", package, "is installed, will install now.")
+            print("[+] It does not seem like", package, "is installed, will install now.")
             subprocess.run(install)
         else:
-            return print(package, "is present.")
+            return print(package, "[+] is present.")
 
 '''if the script importing this function creates an array for the
  variable package we use the above for loop to iterate through all of
