5b79d09592e2452950dbd5853496c0b710546bb6
Gilles <Gillescastro1021@gmail.com>
2025-05-07 22:11:58 -0400

    bash practice

---
+++ bash/rhelPrep/calculator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Write a script that takes two numbers as command-line arguments and prints their sum, difference, product, and quotient.
+
+echo "What is the first number?" && read integer1
+
+echo "What is the second number?" && read integer2
+
+echo "What operation would you like performed: \n"
+
+echo "(usage: add, sub, mul, div, exp,)"
+
+read operation 
+
+if [[ -n $operation ]]; then 
+	echo -e 'We will perform $operator'
+	case "$operation" in
+		add) 
+			let answer=$(($integer1+$integer2))
+			echo $answer
+			;;
+		sub)
+			let answer=$(($interger1+$integer2))
+			echo $answer
+			;;
+		mul)
+			let answer=
+		
+	#elif *
+	#elif **
+	#elif /
+	esac
+fi
