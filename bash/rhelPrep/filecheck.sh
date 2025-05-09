999524e5ef972f208e44ec7eed8f06fec325a2ce
Gilles C <gillescastro1021@gmail.com>
2025-05-08 23:07:36 -0400

    May 8 2025 23:07:36

--- bash/rhelPrep/filecheck.sh
+++ bash/rhelPrep/filecheck.sh
@@ -1,1 +1,3 @@
-#Write a script that checks if a given file exists and is readable. If it exists and is readable, print the first 10 lines of the file. Otherwise, print an appropriate error message.
+#Write a script that checks if a given file exists and is readable. 
+#If it exists and is readable, print the first 10 lines of the file.
+#Otherwise, print an appropriate error message.
