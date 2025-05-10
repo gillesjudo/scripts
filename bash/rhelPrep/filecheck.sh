5e8f0b83c8e24f88b705310cba8b7a07d453df6b
Gilles C <gillescastro1021@gmail.com>
2025-05-09 23:51:49 -0400

    May 9 2025 23:51:49

--- bash/rhelPrep/filecheck.sh
+++ bash/rhelPrep/filecheck.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
 #Write a script that checks if a given file exists and is readable. 
 #If it exists and is readable, print the first 10 lines of the file.
 #Otherwise, print an appropriate error message.
+
+
+home='^\~'
+local='^\.'
+
+echo -e '\nWhat file would you like checked:' 
+read rfile
+echo -e "$rfile"
+if [[ $rfile =~ $home ]]; then
+	rmtil=$(echo "$rfile" | sed 's/\~//g')
+	rfile=$HOME$rmtil
+	echo "$rmtil"
+	echo "$rfile"
+elif [[ $rfile =~ $local ]]; then 
+	rfile=$(pwd)/$(echo "$rfile" | sed 's/\.\///')
+	echo "$rfile"
+fi
