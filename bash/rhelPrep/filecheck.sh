254e8220f9ceee1b75cf13ce2aa8023774c0e932
Gilles C <gillescastro1021@gmail.com>
2025-05-10 17:50:01 -0400

    filecheck features

--- bash/rhelPrep/filecheck.sh
+++ bash/rhelPrep/filecheck.sh
@@ -10,17 +10,43 @@ local='^\.'
 echo -e 'What file would you like checked:' 
 read rfile
 echo "$rfile"
-if [[ $rfile =~ $home && -r $rfile ]]; then
+if [[ $rfile =~ $home ]]; then
 	rmtil=$(echo "$rfile" | sed 's/\~//g')
 	rfile=$HOME$rmtil
-	echo "This file is readable"
-elif [[ $rfile =~ $local && -r $rfile ]]; then 
+elif [[ $rfile =~ $local ]]; then 
 	rfile=$(pwd)/$(echo "$rfile" | sed 's/\.\///')
+fi
+
+if [[ -r $rfile ]]; then 
 	echo "This file is readable"
-elif [[ -r $rfile ]]
-	echo "This file is readable"
-else
-	echo 'This either is not file, not readable or has a bd path.'
-	echo "$(file $rfile && ls -l $rfile)"
-	exit 1
+elif [[ -d $rfile ]]; then 
+	echo  "This is not a file but a directory"
+	echo  "Maybe your file is in here?"
+	contents=$(ls)
+	echo 'Would you like to check one of the contents within?'
+	yes=1
+	no=2
+	answer=""
+	read answer
+	while $answer="";
+		echo 'please answer 1 for yes or 2 for no'
+		if [[ ! $answer -eq $yes || ! $answer -eq $no ]]; then 
+			echo 'Please answer yes or no'
+		elif [[ $answer -eq $yes ]];then
+			echo "$(contents)"
+			echo "Please choose a file"
+			read entity
+			while $entity="";
+				filename="[A-Za-z0-9]+(\.[a-z]+)?"
+				if [[ ! $entity =~ $filename ]]; then
+					entity=""
+					echo "Please enter a proper filename " 
+				elif [[ -r $rfile$entity ]]; then 
+					echo "$entity is readable"
+				elif [[ ! -r $rfile$entity ]]; then
+					echo "This file is not readable"
+
+			elif [[ $answer -eq $no ]]; then 
+			echo "Thank you have a good day!"
+			exit 0
 fi
