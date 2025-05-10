aa3b167671865686c468f6a54b95d18e97044636
Gilles <Gillescastro1021@gmail.com>
2025-05-10 15:46:10 -0400

    update

--- bash/rhelPrep/filecheck.sh
+++ bash/rhelPrep/filecheck.sh
@@ -7,15 +7,20 @@
 home='^\~'
 local='^\.'
 
-echo -e '\nWhat file would you like checked:' 
+echo -e 'What file would you like checked:' 
 read rfile
-echo -e "$rfile"
-if [[ $rfile =~ $home ]]; then
+echo "$rfile"
+if [[ $rfile =~ $home && -r $rfile ]]; then
 	rmtil=$(echo "$rfile" | sed 's/\~//g')
 	rfile=$HOME$rmtil
-	echo "$rmtil"
-	echo "$rfile"
-elif [[ $rfile =~ $local ]]; then 
+	echo "This file is readable"
+elif [[ $rfile =~ $local && -r $rfile ]]; then 
 	rfile=$(pwd)/$(echo "$rfile" | sed 's/\.\///')
-	echo "$rfile"
+	echo "This file is readable"
+elif [[ -r $rfile ]]
+	echo "This file is readable"
+else
+	echo 'This either is not file, not readable or has a bd path.'
+	echo "$(file $rfile && ls -l $rfile)"
+	exit 1
 fi
